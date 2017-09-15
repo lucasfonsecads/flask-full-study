f3eafbadfc397bdede965992897d9db05fc739a5
lucasfonmiranda <lucasfonmiranda@gmail.com>
2017-09-15 10:46:28 -0300

    Functions add in run.py

--- run.py
+++ run.py
@@ -1,27 +1,27 @@
 import flask
-from flask import Flask, render_template, redirect, abort
-from flask_bootstrap import Bootstrap 
+from flask import Flask, render_template, redirect, abort, url_for, session
+from flask_bootstrap import Bootstrap
 from formS import NameForm
 """
 Flask-Bootstrap is imported from the flask.ext name-space and initialized by
 passing the application instance in the constructor
 """
 
-app = Flask(__name__, static_folder='public/assets') 
+app = Flask(__name__, static_folder='public/assets')
 """
 Here we found the unique requered argument to the Flask -> (__name__)
 Here can you see the static_folder is one way to determine your public folder to access
 """
-app.config['SECRET_KEY'] = 'my-precious' 
+app.config['SECRET_KEY'] = 'my-precious'
 """
 Choice one hard string for protect your webapp
 if you like use python in command line generate your key. For this go in README and see the example
 """
-bootstrap = Bootstrap(app) #Here we start the bootstrap application using the app 
+bootstrap = Bootstrap(app) #Here we start the bootstrap application using the app
 
 #========================= FIRST STEP - START FLASK =========================#
 
-@app.route('/') #Hello World for everybody, run you app and show for the world 
+@app.route('/') #Hello World for everybody, run you app and show for the world
 def index():
 	return '<h1>Hello World</h1>'
 
@@ -29,25 +29,25 @@ def index():
 def renderT():
 	return render_template('index.html') #use to render the template with jinja2
 
-@app.route('/user/<name>') 
+@app.route('/user/<name>')
 #Example to use variable in your template with dynamic route.
-#For access go in localhost/user/your-name 
+#For access go in localhost/user/your-name
 def user(name):
 	return render_template('user.html', name=name)
 
 @app.route('/bootstrap/<name>')
-#Example to access your bootstrap template with you put localhost/boostrap/your-name 
+#Example to access your bootstrap template with you put localhost/boostrap/your-name
 #you will see the magic of flask
 def teste(name):
 	return render_template('base.html', name=name)
 
-@app.route('/redirect') #example to use redirect with flask 
+@app.route('/redirect') #example to use redirect with flask
 def redirect():
 	return redirect('https://github.com')
 
 #========================= SECOND STEP  - WEB FORMS =========================#
 
-@app.route('/forms', methods=['GET', 'POST'])
+@app.route('/forms', methods=['GET', 'POST']) #example to use the methods GET and POST
 def formsIndex():
 	name = None
 	form = NameForm()
@@ -56,6 +56,13 @@ def formsIndex():
 		form.name.data = ''
 	return render_template('newindex.html', form=form, name=name)
 
+@app.route('/forms2',  methods=['GET', 'POST'])
+def newform():
+	form = NameForm()
+	if form.validade_on_submit():
+		session['name'] = form.name.data
+		return redirect(url_for('newform'))
+	return render_template('newindex.html', form=form, name=session.get('name'))
 
 
 if __name__ == '__main__':
